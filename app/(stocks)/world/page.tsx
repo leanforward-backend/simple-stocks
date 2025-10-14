75ba908b6b4454ebf209f076e273bb1cef599bde
leanforwarddesign <leanforward.designs@gmail.com>
2025-10-15 10:56:09 +1100

    world map

--- app/(stocks)/world/page.tsx
+++ app/(stocks)/world/page.tsx
@@ -1,9 +1,11 @@
+import { MapComponent } from "@/components/map";
 import { Navbar } from "@/components/navbar";
 
 export default function Page() {
   return (
     <>
       <Navbar />
+      <MapComponent />
       <div className="h-full w-full">hello world</div>
     </>
   );
